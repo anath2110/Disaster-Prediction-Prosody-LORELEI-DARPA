1ce5380a15d5844fefb7cbfa52ae8b4cda20604a
Anindita Nath <32627826+anath2110@users.noreply.github.com>
2022-07-26 02:41:26 -0600

    Delete Codes/Linear Regression directory

--- Codes/Linear Regression/sfFieldName.m
+++
@@ -1,9 +0,0 @@
-
-function fieldString = sfFieldName(i)
-  [fieldStdNames, typeStdNames] = sfNamings();
-  if i <= length(fieldStdNames)
-    fieldString = fieldStdNames(i);
-  else
-    fieldString = typeStdNames(i-6);
-  end
-end
